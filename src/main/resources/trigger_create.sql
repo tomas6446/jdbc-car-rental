927f8e95fa6bd804e45e4070fb5846bd2af51d53
tomas <tomas.kozakas@gmail.com>
2023-04-27 11:32:31 +0300

    Replace to simplified version of trigger

--- src/main/resources/trigger_create.sql
+++ src/main/resources/trigger_create.sql
@@ -4,18 +4,10 @@ DROP TRIGGER IF EXISTS trg_update_amount_paid ON rent;
 CREATE OR REPLACE FUNCTION update_amount_paid()
     RETURNS TRIGGER AS
 $$
-DECLARE
-    car_daily_rate DECIMAL;
 BEGIN
-    -- Get the daily_rate of the rented car
-    SELECT daily_rate
-    INTO car_daily_rate
-    FROM Car
-    WHERE Car.car_id = NEW.car_id;
-
-    -- Update the amount_paid based on the daily_rate and the rental duration (return_date - rent_date)
-    NEW.amount_paid := car_daily_rate * (NEW.return_date - NEW.rent_date);
-
+    NEW.amount_paid = (SELECT daily_rate
+                       FROM Car
+                       WHERE car_id = NEW.car_id) * (NEW.return_date - NEW.rent_date);
     RETURN NEW;
 END;
 $$ LANGUAGE plpgsql;
