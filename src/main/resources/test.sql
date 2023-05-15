b4089ff6232e7a7006ed1da3f10f2b15864532e5
tomas <tomas.kozakas@gmail.com>
2023-05-15 21:39:31 +0300

    Update tests

--- src/main/resources/test.sql
+++ src/main/resources/test.sql
@@ -1,66 +1,66 @@
 -- Tables
 SELECT *
-FROM customer;
+FROM toko7940.customer;
 SELECT *
-FROM reservation;
+FROM toko7940.reservation;
 SELECT *
-FROM rent;
+FROM toko7940.rent;
 SELECT *
-FROM car;
+FROM toko7940.car;
 
 -- Test unique index by trying to insert an existing email
-INSERT INTO Customer (name, email, phone)
+INSERT INTO toko7940.Customer (name, email, phone)
 VALUES ('Tomas Kozakas', 'tomas.kozakas@gmail.com', '13333222');
 -- Test index by sorting rents by rent_date in desc order
 SELECT *
-FROM Rent
+FROM toko7940.Rent
 ORDER BY rent_date DESC;
 
 -- Test view
 SELECT *
-FROM customer_rentals;
+FROM toko7940.customer_rentals;
 SELECT *
-FROM car_reservations;
+FROM toko7940.car_reservations;
 SELECT *
-FROM car_rentals;
+FROM toko7940.car_rentals;
 
 -- Test materialized view
 SELECT *
-FROM car_rental_stats;
+FROM toko7940.car_rental_stats;
 
-INSERT INTO Rent (car_id, customer_id, rent_date, return_date, amount_paid)
+INSERT INTO toko7940.Rent (car_id, customer_id, rent_date, return_date, amount_paid)
 VALUES (1, 1, '2023-04-10', '2023-04-14', 260.00);
 
-REFRESH MATERIALIZED VIEW car_rental_stats;
+REFRESH MATERIALIZED VIEW toko7940.car_rental_stats;
 
 SELECT *
-FROM car_rental_stats;
+FROM toko7940.car_rental_stats;
 
 -- Test triggers
 -- Bandom nuomoti masina kuri jau yra rezervuota
-INSERT INTO Reservation (car_id, customer_id, reservation_date, expiration_date)
+INSERT INTO toko7940.Reservation (car_id, customer_id, reservation_date, expiration_date)
 VALUES (1, 4, '2023-05-04', '2023-05-30');
 SELECT *
-FROM reservation;
-INSERT INTO Rent (car_id, customer_id, rent_date, return_date)
+FROM toko7940.reservation;
+INSERT INTO toko7940.Rent (car_id, customer_id, rent_date, return_date)
 VALUES (1, 5, '2023-05-04', '2023-05-30');
 
 -- Bandom insertint masina kuri yra 1799 metu
-INSERT INTO Car (manufacturer, model, year, daily_rate)
+INSERT INTO toko7940.Car (manufacturer, model, year, daily_rate)
 VALUES ('Car', 'car', 1799, 0);
 
 -- Bandom inserting Rent ar Reservation kuriu datos nesutampa
-INSERT INTO Rent (car_id, customer_id, rent_date, return_date, amount_paid)
+INSERT INTO toko7940.Rent (car_id, customer_id, rent_date, return_date, amount_paid)
 VALUES (1, 1, '2023-04-14', '2023-04-10', 260.00);
-INSERT INTO Reservation (car_id, customer_id, reservation_date, expiration_date)
+INSERT INTO toko7940.Reservation (car_id, customer_id, reservation_date, expiration_date)
 VALUES (1, 4, '2023-04-14', '2023-04-10');
 
 -- Test kiek uzmuokejo
-INSERT INTO Rent (car_id, customer_id, rent_date, return_date)
+INSERT INTO toko7940.Rent (car_id, customer_id, rent_date, return_date)
 VALUES (1, 5, '2023-03-10', '2023-04-25');
 SELECT *
-FROM Rent;
+FROM toko7940.Rent;
 
 -- Test non valid phone number
-INSERT INTO Customer (name, email, phone)
+INSERT INTO toko7940.Customer (name, email, phone)
 VALUES ('Tomas Kozakas', 'tomaskozakas@gmail.com', '3767217198');
