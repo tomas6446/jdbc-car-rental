fe62eac8bde432b9bc50c3c1c8065840877fa758
tomas <tomas.kozakas@gmail.com>
2023-05-15 22:55:54 +0300

    Add mapper

--- src/main/java/com/jdbc/carrental/repository/ReservationRepository.java
+++ src/main/java/com/jdbc/carrental/repository/ReservationRepository.java
@@ -1,71 +1,64 @@
 package com.jdbc.carrental.repository;
 
 import com.jdbc.carrental.connection.DatabaseConnection;
+import com.jdbc.carrental.mapper.ReservationMapper;
 import com.jdbc.carrental.model.Reservation;
 
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * @author Tomas Kozakas
  */
 public class ReservationRepository extends BaseRepository<Reservation> {
+    private final ReservationMapper reservationMapper;
 
-    public ReservationRepository(DatabaseConnection databaseConnection) {
+    public ReservationRepository(DatabaseConnection databaseConnection, ReservationMapper reservationMapper) {
         super(databaseConnection);
+        this.reservationMapper = reservationMapper;
     }
 
     @Override
-    public List<Reservation> getAll() {
-        return executeQuery("SELECT * FROM reservation", resultSet -> {
-            Reservation reservation = new Reservation();
-            try {
-                reservation.setReservationId(resultSet.getInt("reservation_id"));
-                reservation.setCarId(resultSet.getInt("car_id"));
-                reservation.setCustomerId(resultSet.getInt("customer_id"));
-                reservation.setReservationDate(resultSet.getDate("reservation_date"));
-                reservation.setExpirationDate(resultSet.getDate("expiration_date"));
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
-            return reservation;
-        });
+    public List<Reservation> getAll() throws SQLException {
+        return executeQuery("SELECT * FROM reservation", reservationMapper::map);
     }
 
     @Override
-    public void enter(Reservation reservation) {
-        String query = "INSERT INTO reservation (car_id, customer_id, reservation_date, expiration_date) " +
-                "VALUES (" + reservation.getCarId() + ", " + reservation.getReservationId() + ", '" + reservation.getReservationDate() +
-                "', '" + reservation.getExpirationDate() + "')";
-
-        executeInsert(query);
+    public void enter(Reservation reservation) throws SQLException {
+        executeInsert("INSERT INTO reservation (car_id, customer_id, reservation_date, expiration_date) " +
+                "VALUES (" + reservation.getCarId() + ", " + reservation.getCustomerId() + ", '" + reservation.getReservationDate() +
+                "', '" + reservation.getExpirationDate() + "')");
     }
 
     @Override
-    public void search() {
-
+    public List<Reservation> search(SearchParam searchParam) throws SQLException {
+        return executeQuery("SELECT * FROM reservation WHERE " + searchParam.column() + searchParam.like(), reservationMapper::map);
     }
 
+
     @Override
-    public void update(int id, Reservation reservation) {
-        String query = "UPDATE Reservation SET " +
+    public void update(int id, Reservation reservation) throws SQLException {
+        executeUpdate("UPDATE reservation SET " +
                 "car_id = " + reservation.getCarId() +
                 ", customer_id = " + reservation.getCustomerId() +
                 ", reservation_date = '" + reservation.getReservationDate() + "'" +
                 ", expiration_date = '" + reservation.getExpirationDate() + "'" +
-                " WHERE reservation_id = " + id;
-        executeUpdate(query);
+                " WHERE reservation_id = " + id);
     }
 
     @Override
-    public void delete(int id) {
+    public void delete(int id) throws SQLException {
         executeDelete("reservation", "reservation_id", id);
     }
 
     @Override
-    public Reservation ask(Scanner scanner) {
-        return null;
-    }
+    public Reservation askInsert() {
+        System.out.print("Car id: ");
+        int carId = scanner.nextInt();
+        System.out.print("Customer id: ");
+        int customerId = scanner.nextInt();
 
+        DateInput date = getDate(scanner);
+        return new Reservation(carId, customerId, date.startDate(), date.endDate());
+    }
 }
