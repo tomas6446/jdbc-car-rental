618b85035a64ab166122641b348e830b601c7bc8
tomas <tomas.kozakas@gmail.com>
2023-05-15 22:55:48 +0300

    Add header

--- src/main/java/com/jdbc/carrental/printer/TablePrinter.java
+++ src/main/java/com/jdbc/carrental/printer/TablePrinter.java
@@ -9,13 +9,13 @@ import java.util.List;
 public class TablePrinter {
     public static <T extends PrintableTable> void printTable(String title, List<T> items) {
         System.out.println(title);
+        System.out.println(items.get(0).header());
         if (items.isEmpty()) {
             System.out.println("**Is empty**\n");
         } else {
             for (T item : items) {
                 System.out.println(item.toTableRow());
             }
-            System.out.println("\n");
         }
 
     }
