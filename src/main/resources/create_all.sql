646b515eb9460c33ec60d91c657a80c81c5a3d4f
tomas <tomas.kozakas@gmail.com>
2023-06-14 21:57:29 +0300

    3nd lab commit

--- src/main/resources/create_all.sql
+++ src/main/resources/create_all.sql
@@ -157,7 +157,7 @@ VALUES (1, 1, '2023-04-10', '2023-04-14', 260.00),
        (2, 3, '2023-04-12', '2023-04-16', 240.00),
        (3, 2, '2023-04-13', '2023-04-16', 255.00);
 
-INSERT INTO 1Reservation (car_id, customer_id, reservation_date, expiration_date)
+INSERT INTO Reservation (car_id, customer_id, reservation_date, expiration_date)
 VALUES (1, 4, '2023-04-18', '2023-04-25'),
        (2, 2, '2023-04-15', '2023-04-20'),
        (3, 3, '2023-04-17', '2023-04-20');
